b88debb7f1e512c8c9402c186f224ef4137439f2
Agustín Fara <agustinfara@gmail.com>
2020-12-16 18:32:19 -0300

    se quita secret key

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q51j__&b!ojnj^%y19ot90_vqyf@(y1jxj-i@@$i*&huvx8=#l'
+#Leo config.ini file
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
