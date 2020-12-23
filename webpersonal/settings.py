d57322dbcf8be010546218c39411dd14ab498f43
Agustín Fara <agustinfara@gmail.com>
2020-12-22 21:34:37 -0300

    mod settings.py

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = ( BASE_DIR / 'static')
+STATIC_ROOT = ( BASE_DIR / 'static')
 
 #Media Files
 MEDIA_URL = '/media/'
